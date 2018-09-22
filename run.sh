52eb44897af534603c3a406372e53654c58bcb2a
Dushyant Singh <dush1729@gmail.com>
2018-09-22 18:02:46 +0530

    Remove binary file

--- run.sh
+++ run.sh
@@ -4,9 +4,19 @@ count=0
 g++ main.cpp -std=c++11 -fopenmp
 echo "              Number             Serial Time           Parallel Time                Speed Up"
 
-while [ $count -lt 18 ]
+while true
 do
 	./a.out $number
-	number=`expr $number \* 10`
 	count=`expr $count + 1`
+	
+	# We could simply have written while loop with condition on count < 19
+	# but with that solution value of number will overflow
+	if [ $count -eq 19 ]
+	then
+		break
+	fi
+
+	number=`expr $number \* 10`
 done
+
+rm a.out
